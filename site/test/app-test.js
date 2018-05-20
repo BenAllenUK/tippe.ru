953224a8f29657b07afb30695bd44fc55f3b26e8
Nick Pearson <nicholas.pearson4@btinternet.com>
2018-05-20 17:05:33 +0100

    Fix for websocket server leaving processes hanging in the background keeping the app open forever

--- site/test/app-test.js
+++ site/test/app-test.js
@@ -4,16 +4,13 @@ var expect = chai.expect;
 // set of integration tests for the main server app
 describe('Integration', function()
 {
-/*  it('Server starts up and shuts down gracefully', function()
+  it('Server starts up and shuts down gracefully', function(done)
   {
     this.slow(400);
 
     var server = require('../www');
-
-    expect(server.webserver);
-    expect(server.socketserver);
-
-    server.webserver.close();
-    server.socketserver.close();
-  });*/
+    server.close(function() {
+      done();
+    });
+  });
 });
